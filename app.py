951d72306b2dadb4e8477eeb0670797956f9b5e1
Jacob <dirjacob@me.com>
2024-07-24 13:51:22 +0200

    New Version for less CPU consumption

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 import os
 import datetime
 import random
-import time
 import influxdb_client
 from influxdb_client import InfluxDBClient, Point, WritePrecision, WriteOptions
-from influxdb_client.client.write_api import SYNCHRONOUS
 import numpy as np
 import asyncio
 
@@ -12,28 +10,21 @@ def random_hex_string(length=4):
     return os.urandom(length).hex()
 
 def generate_measurement(name, timestamp, length):
-    ret_dict = {"measurement": name, "time": timestamp}
-    gen_fields = {f"f-{i}": random.uniform(0.1, 100.0) for i in range(length)}
-    ret_dict["fields"] = gen_fields
-    return ret_dict
+    return {
+        "measurement": name,
+        "time": timestamp,
+        "fields": {f"f-{i}": random.uniform(0.1, 100.0) for i in range(length)}
+    }
 
 def get_pump_count():
-    env = 'PUMPCOUNT'
-    pump_count = 20
-    if env in os.environ:
-        pump_count_str = os.environ[env]
-        if pump_count_str.isnumeric:
-            pump_count = int(pump_count_str)
-        if pump_count == 0:
-            pump_count = 1
-    return pump_count
+    pump_count = int(os.getenv('PUMPCOUNT', 20))
+    return max(pump_count, 1)
 
 def generate_pump(number):
     lauf = f"-LC{number}"
-    sensors = [(random_hex_string() + lauf, 1) for _ in range(7)] + \
-              [(random_hex_string() + lauf, 30) for _ in range(3)] + \
-              [(random_hex_string() + lauf, 5) for _ in range(2)]
-    return sensors
+    return [(random_hex_string() + lauf, 1) for _ in range(7)] + \
+           [(random_hex_string() + lauf, 30) for _ in range(3)] + \
+           [(random_hex_string() + lauf, 5) for _ in range(2)]
 
 def generate_pump_list(pump_count):
     return [generate_pump(i) for i in range(pump_count)]
@@ -51,8 +42,8 @@ async def main():
 
     client = InfluxDBClient(url=url, token=token, org=org)
     write_options = WriteOptions(
-        batch_size=1500,             # Große Batch Size, um die Anzahl der HTTP-Anfragen zu reduzieren
-        flush_interval=1500,         # Flush Interval von 1,5 Sekunden, um die Latenz zu verringern
+        batch_size=5000,             # Große Batch Size, um die Anzahl der HTTP-Anfragen zu reduzieren
+        flush_interval=3000,         # Flush Interval von 3 Sekunden, um die Latenz zu verringern
         jitter_interval=500,         # Jitter Interval, um Überlastungen zu vermeiden
         retry_interval=1500,         # Retry Interval von 1,5 Sekunden
         max_retries=5,               # Maximale Anzahl von 5 Wiederholungsversuchen
@@ -62,39 +53,37 @@ async def main():
 
     write_api = client.write_api(write_options=write_options)
     log = False  # Set to True for debugging
-
     pump_count = get_pump_count()
-    print_log(log, f"Pump_count = {pump_count}")
-
-    print_log(log, "Start Pump generation")
     pump_list = generate_pump_list(pump_count)
-    print_log(log, "End Pump generation")
 
     while True:
         dt = datetime.datetime.now()
         tasks = []
 
         for pump in pump_list:
-            for sensor in pump:
-                tasks.append(process_sensor(write_api, bucket, org, sensor, dt, log))
+            tasks.append(process_pump(write_api, bucket, org, pump, dt, log))
 
         await asyncio.gather(*tasks)
-        print_log(log, "Completed a cycle of sensor data generation.")
         await asyncio.sleep(1)  # Hauptschleife wird alle 1 Sekunde durchlaufen
 
+async def process_pump(write_api, bucket, org, pump, dt, log):
+    tasks = []‘
+    for sensor in pump:
+        tasks.append(process_sensor(write_api, bucket, org, sensor, dt, log))
+    await asyncio.gather(*tasks)
+
 async def process_sensor(write_api, bucket, org, sensor, dt, log):
-    records = []
-    for _ in range(sensor[1]):
-        records.append(generate_measurement(sensor[0], dt, sensor[1]))
-        if sensor[1] == 1:
-            await asyncio.sleep(abs(np.random.normal(1, 0.2)))  # Normal distribution for 1 Hz
-        elif sensor[1] == 30:
-            await asyncio.sleep(abs(np.random.exponential(1/30)))  # Exponential distribution for 30 Hz
-        elif sensor[1] == 5:
-            await asyncio.sleep(abs(np.random.normal(1/5, 0.1)))  # Normal distribution for 5 Hz
+    records = [generate_measurement(sensor[0], dt, sensor[1]) for _ in range(sensor[1])]
+    
+    if sensor[1] == 1:
+        await asyncio.sleep(abs(np.random.normal(1, 0.2)))  # Normal distribution for 1 Hz
+    elif sensor[1] == 30:
+        await asyncio.sleep(abs(np.random.exponential(1/30)))  # Exponential distribution for 30 Hz
+    elif sensor[1] == 5:
+        await asyncio.sleep(abs(np.random.normal(1/5, 0.1)))  # Normal distribution for 5 Hz
 
     b_w = datetime.datetime.now()
-    write_api.write(bucket, org, records, write_precision="ns")
+    write_api.write(bucket, org, records, write_precision=WritePrecision.NS)
     a_w = datetime.datetime.now()
     if log:
         print(f"{sensor[0]} | {b_w} | {a_w}")
